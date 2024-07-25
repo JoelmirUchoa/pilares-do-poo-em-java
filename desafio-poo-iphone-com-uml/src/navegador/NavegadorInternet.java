0c7bbca187ecfa2f447323138c5fea0ca568c347
JoelmirUchoa <149214348+JoelmirUchoa@users.noreply.github.com>
2024-07-24 23:54:34 -0300

    Delete desafio-poo-iphone-com-uml directory

--- desafio-poo-iphone-com-uml/src/navegador/NavegadorInternet.java
+++
@@ -1,7 +0,0 @@
-package navegador;
-
-public interface NavegadorInternet {
-    public void exibirPagina(String url);
-    public void adicionarNovaAba();
-    public void atualizarPagina();
-}
--- desafio-poo-iphone-com-uml/src/telefone/AparelhoTelefonico.java
+++
@@ -1,7 +0,0 @@
-package telefone;
-
-public interface AparelhoTelefonico {
-    public void ligar(String numero);
-    public void atender();
-    public void iniciarCorreioVoz();
-}
